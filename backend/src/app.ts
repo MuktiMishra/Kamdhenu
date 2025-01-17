116a0843cf3a6de00ef70c92cc312d01cf9cc1d8
Krypton321123 <coolrajsharma28@gmail.com>
2025-01-17 16:20:36 +0530

    deployment commit -2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import cors from 'cors'
 import userRouter from "./routes/user.router.js";
 import adminRouter from "./routes/admin.router.js";
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: '16kb'}));
+
+app.get("/", async (req: Request, res: Response) => {
+    res.status(200).json({message: "service is running"})
+})
 
 app.use('/api/user', userRouter)
 app.use('/api/admin', adminRouter);
