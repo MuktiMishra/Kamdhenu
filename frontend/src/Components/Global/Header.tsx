9774973a194a694fc565af732dd14d78dbc5ce8a
Krypton321123 <coolrajsharma28@gmail.com>
2025-01-17 16:45:46 +0530

    deployement commit -3

--- frontend/src/Components/Global/Header.tsx
+++ frontend/src/Components/Global/Header.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 interface HeaderProps {
     children?: React.ReactNode;
 }
 
 const Header : React.FC<HeaderProps> = () => {
     const [isMenuOpen , setIsMenuOpen] = useState(false);
+    const navigate = useNavigate(); 
     return(
         <div className='sticky top-0 z-10 bg-white shadow-md'>
             <div 
@@ -37,54 +38,54 @@ const Header : React.FC<HeaderProps> = () => {
                 text-4xl cursor-pointer' onClick={()=>(setIsMenuOpen(!isMenuOpen))}>      
                 </i>
 
-                <div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform 
+                {isMenuOpen && (<div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform 
                 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}
                 style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}>
                     <div className="flex justify-center gap-4 my-4">
-                        <button className="bg-[#B0C4DE] rounded-lg hover:bg-blue-500 hover:text-white text-lg h-10 w-24">
+                        <button onClick={() => {setIsMenuOpen(false); navigate('/login')}} className="bg-[#B0C4DE] rounded-lg hover:bg-blue-500 hover:text-white text-lg h-10 w-24">
                           Login
                         </button>
-                        <button className="bg-[#B0C4DE] rounded-lg hover:bg-blue-500 hover:text-white text-lg h-10 w-24">
+                        <button onClick={() => {setIsMenuOpen(false); navigate('/signup')}} className="bg-[#B0C4DE] rounded-lg hover:bg-blue-500 hover:text-white text-lg h-10 w-24">
                           Signup
                         </button>
                     </div>
                     <Link to="/">
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li onClick={() => setIsMenuOpen(false)} className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                     Home
                     </li>
                     </Link>
                     
                     <Link to="/aboutus">
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li onClick={() => setIsMenuOpen(false)} className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                         About Us
                     </li>
                     </Link>
                     
                     <Link to="/programs">
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li onClick={() => setIsMenuOpen(false)} className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                         Programs
                     </li>
                     </Link>
                      
                     <Link to='/gallery'>
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li onClick={() => setIsMenuOpen(false)} className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                         Our Gallery
                     </li>
                     </Link>
                     
                     <Link to='/testimonials'>
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li onClick={() => setIsMenuOpen(false)} className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                         Testimonials
                     </li>
                     </Link>
                     
                     <Link to='/contactus'>
-                    <li className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
+                    <li  className='list-none w-full text-center p-4 hover:bg-blue-500 hover:text-white transition-all cursor-pointer'>
                         Contact Us
                     </li>
                     </Link>
                 </div>
-                
+                )}
             </div>
             
         </div>
