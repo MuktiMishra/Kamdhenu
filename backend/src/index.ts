b903cd92bacf4ef0e825c69052fd8ca97903dcf6
Krypton321123 <coolrajsharma28@gmail.com>
2025-01-20 21:05:18 +0530

    deployment ready backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import app from './app.js'
+import https from 'https'
+import fs from 'fs'
 
 
 
@@ -8,10 +10,18 @@ dotenv.config();
 const start = () => {
     try {
 
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on http://localhost:${process.env.PORT}`)
-        })
+        const port = process.env.PORT as any || 4000
 
+        const options = {
+            key: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/privkey.pem"),
+            cert: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/fullchain.pem")
+        }
+
+        const displayURL = `https://server.kamdhenuskills.com:${port}`
+
+        https.createServer(options, app).listen(port, '0.0.0.0', () => {
+            console.log(`Server listening on ${displayURL}`)
+        })
 
     } catch (err) {
         console.log(`Error in starting server ===> ${err}`)
