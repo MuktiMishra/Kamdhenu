18dcda4ff43f64480b5b6bdb833dc496de8d447b
Krypton321123 <coolrajsharma28@gmail.com>
2025-02-06 21:36:14 +0530

    added stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,19 +12,19 @@ const start = () => {
 
         const port = process.env.PORT as any || 4000
 
-        // const options = {
-        //     key: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/privkey.pem"),
-        //     cert: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/fullchain.pem")
-        // }
+        const options = {
+            key: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/privkey.pem"),
+            cert: fs.readFileSync("/etc/letsencrypt/live/server.kamdhenuskills.com/fullchain.pem")
+        }
 
         const displayURL = `https://server.kamdhenuskills.com:${port}`
 
-        app.listen(port, () => {
-            console.log(`Server is listening at http://localhost:${port}`)
-        })
-        // https.createServer(options, app).listen(port, '0.0.0.0', () => {
-        //     console.log(`Server listening on ${displayURL}`)
+        // app.listen(port, () => {
+        //     console.log(`Server is listening at http://localhost:${port}`)
         // })
+        https.createServer(options, app).listen(port, '0.0.0.0', () => {
+            console.log(`Server listening on ${displayURL}`)
+        })
 
     } catch (err) {
         console.log(`Error in starting server ===> ${err}`)
